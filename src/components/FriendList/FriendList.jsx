2b113444cb177776e5eb66bc56683feac313f1c9
MykolaHrytsenko <niksandler@gmail.com>
2022-07-23 22:05:34 +0300

    add 3d task

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import css from './FriendList.module.css';
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
 
-export const FriendList = () => {
+export const FriendList = ({ friends }) => {
   return (
-    <ul class="friend-list">
-      {/* <!-- Довільна кіл-сть FriendListItem --> */}
+    <ul className={css.friendList}>
+      {friends.map(friend => (
+        <li className={css.item} key={friend.id}>
+          <span className={friend.isOnline ? css.onLine : css.offLine}></span>
+          <img
+            className={css.avatar}
+            src={friend.avatar}
+            alt="User avatar"
+            width="52"
+          />
+          <p className={css.name}>{friend.name}</p>
+        </li>
+      ))}
     </ul>
   );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
 };
