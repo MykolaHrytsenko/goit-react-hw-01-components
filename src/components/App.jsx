27969ea76c6487c87cb7b7bf41f1655ab8300428
MykolaHrytsenko <niksandler@gmail.com>
2022-07-23 19:44:14 +0300

    test

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import { Profile } from './Profile/Prolife';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+
+import user from '../data/user.json';
+import statistics from '../data/statistics.json';
+import friends from '../data/friends.json';
+
+export const App = () => {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statistics} />
+      <FriendList friends={friends} />;
+    </>
+  );
+};
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import css from './FriendList.module.css';
+
+export const FriendList = () => {
+  return (
+    <ul class="friend-list">
+      {/* <!-- Довільна кіл-сть FriendListItem --> */}
+    </ul>
+  );
+};
